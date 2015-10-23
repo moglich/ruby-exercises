036717a39a32f04679ae169df264f04a25f44392
Remo Regli <remo.regli@gmail.com>
2015-10-23 15:25:45 +0200

    updated getter and setter methodes, refactored functions

--- simulator/simulator.rb
+++ simulator/simulator.rb
@@ -1,51 +1,49 @@
 class Robot
   DIRECTIONS = [:north, :east, :south, :west]
-  attr_accessor :bearing, :direction, :x, :y
+  attr_accessor :bearing, :x, :y
 
   def initialize
-    self.x = 0
-    self.y = 0
+    @x = 0
+    @y = 0
   end
 
   def orient(direction)
     input_valid?(direction)
-    @bearing = direction 
+    self.bearing = direction 
   end
 
   def turn_right
-    new_index = DIRECTIONS.index(@bearing) + 1
-    new_index = 0 if new_index > 3
-    @bearing = DIRECTIONS[new_index]
+    index = (DIRECTIONS.index(self.bearing) + 1) % 4
+    self.bearing = DIRECTIONS[index]
   end
 
   def turn_left
-    new_index = DIRECTIONS.index(@bearing) - 1
-    new_index = 3 if new_index < 0
-    @bearing = DIRECTIONS[new_index]
+    index = (DIRECTIONS.index(self.bearing) - 1) % 4
+    self.bearing = DIRECTIONS[index]
   end
 
   def advance
-    case @bearing
-    when :north then @y += 1 
-    when :east  then @x += 1 
-    when :south then @y -= 1 
-    when :west  then @x -= 1 
+    case self.bearing
+    when DIRECTIONS[0] then self.y += 1 
+    when DIRECTIONS[1] then self.x += 1 
+    when DIRECTIONS[2] then self.y -= 1 
+    when DIRECTIONS[3] then self.x -= 1 
     end
   end
 
   def at(cord_x, cord_y)
-    @x = cord_x
-    @y = cord_y
+    self.x = cord_x
+    self.y = cord_y
   end
 
   def coordinates
-    [@x, @y]
+    [self.x, self.y]
   end
 
   private
 
   def input_valid?(input)
-    raise ArgumentError if not [:east, :west, :north, :south].include? input
+    raise ArgumentError if not DIRECTIONS.include? input
   end
 end
 
