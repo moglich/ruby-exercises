aa635c7d1c4919b6ee19b8606dc972879d2c672d
Remo Regli <remo.regli@gmail.com>
2015-11-12 16:15:08 +0100

    added missing ','

--- series/series.rb
+++ series/series.rb
@@ -4,7 +4,7 @@ class Series
   end
 
   def slices(slice_size)
-    raise ArgumentError "Slice size is bigger than string!" if slice_size > @string_slices.size
+    raise ArgumentError, "Slice size is bigger than string!" if slice_size > @string_slices.size
     series = []
     @string_slices.size.times do
       if @string_slices.size >= slice_size
