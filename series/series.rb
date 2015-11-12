59fb0a0b3a5d2a146b100b74991e4102072cccb1
Remo Regli <remo.regli@gmail.com>
2015-11-12 15:46:52 +0100

    added series exercise - first draft

---
+++ series/series.rb
@@ -0,0 +1,17 @@
+class Series
+  def initialize(string)
+    @string = string.chars.map { |c| c.to_i }
+  end
+
+  def slices(slice_size)
+    raise ArgumentError if slice_size > @string.size
+    series = []
+    @string.size.times do
+      if @string.size >= slice_size
+        series << @string.first(slice_size)
+        @string.shift
+      end
+    end
+    series
+  end
+end
