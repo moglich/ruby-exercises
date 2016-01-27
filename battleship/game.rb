6e5dc755f201ff5aaed0c228f402ce57314620e8
Remo Regli <remo.regli@gmail.com>
2016-01-27 08:09:00 +0100

    updated grid

--- battleship/game.rb
+++ battleship/game.rb
@@ -1,38 +1,89 @@
 class Board
-  attr_accessor :grid
+  attr_accessor :board_layout
+  ROWS    = 5 # max 9
+  COLUMNS = 5 # max 9
 
   def initialize
-    @grid = {}
-    (1..25).each { |pos| @grid[pos] = ' ' }
+    @board_layout = Array.new(ROWS, " ").map{|row| Array.new(COLUMNS, " ")}
   end
 
   def show
     system 'clear'
-    puts "----  Battleship Game  ----"
+
+    print "    "
+    COLUMNS.times {|col| print " #{col + 1}  " }
     puts
-    puts "    1   2   3   4   5"
-    puts "  +---+---+---+---+---+"
-    puts "1 | #{@grid[1]} | #{@grid[2]} | #{@grid[3]} | #{@grid[4]} | #{@grid[5]} |"
-    puts "  +---+---+---+---+---+"
-    puts "2 | #{@grid[6]} | #{@grid[7]} | #{@grid[8]} | #{@grid[9]} | #{@grid[10]} |"
-    puts "  +---+---+---+---+---+"
-    puts "3 | #{@grid[11]} | #{@grid[12]} | #{@grid[13]} | #{@grid[14]} | #{@grid[15]} |"
-    puts "  +---+---+---+---+---+"
-    puts "4 | #{@grid[16]} | #{@grid[17]} | #{@grid[18]} | #{@grid[19]} | #{@grid[20]} |"
-    puts "  +---+---+---+---+---+"
-    puts "5 | #{@grid[21]} | #{@grid[22]} | #{@grid[23]} | #{@grid[24]} | #{@grid[25]} |"
-    puts "  +---+---+---+---+---+"
-    puts ""
+    print "   +"
+    COLUMNS.times { print "---+" }
+    puts
+
+    ROWS.times do |row|
+      print " #{row + 1} |"
+      COLUMNS.times do |column|
+        print " #{board_layout[row][column]} |"
+      end
+      puts
+      print "   +"
+      COLUMNS.times { print "---+" }
+      puts
+    end
+    puts @display_msg
   end
 
   def get_empty_positions
-    @grid.keys.select {|position| @grid[position] == ' '}
+    @board_layout.each.select {|position| @board_layout[position] == ' '}
+  end
+
+  def print_layout
+    puts @board_layout.inspect
+  end
+
+  def set_field(x = 0, y = 0, mark)
+    x -= 1
+    y -= 1
+    if ((0...ROWS).include? x) and ((0...COLUMNS).include? y)
+      if @board_layout[x][y] == " "
+        @board_layout[x][y] = mark
+        @display_msg = "Field choosen!"
+      else
+        @display_msg = "Choose another field!"
+      end
+    else
+      @display_msg = "Selection out of range!"
+    end
+  end
+
+  def place_ships
+    @placed_ships = Array.new(ROWS, " ").map{|row| Array.new(COLUMNS, " ")}
+    @placed_ships[0][0] = "x"
   end
 end
 
-loop do
-  board = Board.new
-  board.grid[board.get_empty_positions.sample] = "X"
-  board.show
-  sleep 1
+class Player
+  def initialize
+    @board = Board.new
+  end
 end
+
+class Game
+  def initialize
+    @board = Board.new
+  end
+
+  def play
+    loop do
+      @board.show
+      get_user_input
+      #pseudo code
+      # detect hit
+      # 
+    end
+  end
+
+  def get_user_input
+    input = gets.chomp
+    position = input.split(',')
+    @board.set_field(position[0].to_i, position[1].to_i, 'X')
+  end
+end
+
