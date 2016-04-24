561aae155d2d958bb41b51f0c4322a1028703f53
Remo Regli <remo.regli@gmail.com>
2016-04-24 17:55:22 +0200

    solution without sort

--- anagrams/anagram.rb
+++ anagrams/anagram.rb
@@ -1,18 +1,25 @@
+require 'pry'
 class Anagram
   def initialize(origin_word)
     @origin_word = origin_word
   end
 
   def match(words)
-    anagrams = []
+    words.select { |word| anagram? @origin_word, word }
+  end
 
-    words.each do |word|
-      next unless word.length == @origin_word.length
-      if word.downcase.chars.sort == @origin_word.downcase.chars.sort
-        anagrams << word unless word.downcase == @origin_word.downcase
-      end
+  private
+
+  def anagram?(original_word, possible_anagram)
+    return false if original_word.downcase == possible_anagram.downcase
+    return false if original_word.length != possible_anagram.length
+
+    match_word = possible_anagram.downcase.chars
+
+    original_word.downcase.chars.each do |letter|
+      index = match_word.index(letter)
+      match_word.delete_at(index) if index
     end
-
-    anagrams
+    match_word.empty?
   end
 end
