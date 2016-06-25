04ad458cb1bd0520489c497d45f6e4cf4e64e422
Remo Regli <remo.regli@gmail.com>
2016-06-25 08:36:01 +0200

    simplify decode method

--- run_length_encoding/run_length_encoding.rb
+++ run_length_encoding/run_length_encoding.rb
@@ -25,13 +25,10 @@ class RunLengthEncoding
         if /\d/.match input[idx + 1]
           char_count = (char_count + char.to_i) * 10
         else
-          char_count = char_count + char.to_i
+          char_count += char.to_i
         end
-        #require 'pry'; binding.pry
       else
-        if char != input[idx + 1] && char_count == 0
-          char_count = 1
-        end
+        char_count = 1 if char_count == 0
         output << (char * char_count)
         char_count = 0
       end
