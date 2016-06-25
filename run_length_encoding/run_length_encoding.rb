8ab0da6c389820df7e160209e8b7ab745619d500
Remo Regli <remo.regli@gmail.com>
2016-06-25 10:38:19 +0200

    use matched groups for output

--- run_length_encoding/run_length_encoding.rb
+++ run_length_encoding/run_length_encoding.rb
@@ -1,14 +1,13 @@
 class RunLengthEncoding
   def self.encode(input)
     input.gsub(/((.)\2*)/) do |part|
-      part.size == 1 ? part[0] : (part.size.to_s + part[0])
+      part.size == 1 ? $2 : (part.size.to_s + $2)
     end
   end
 
   def self.decode(input)
-    input.gsub(/\d*./) do |part|
-      count = part.match(/\d*/)[0].to_i
-      count == 0 ? part : part.match(/[^\d]/).to_s * count
+    input.gsub(/(\d*)(.)/) do
+      $1.to_i == 0 ? $2 : $2 * $1.to_i
     end
   end
 end
